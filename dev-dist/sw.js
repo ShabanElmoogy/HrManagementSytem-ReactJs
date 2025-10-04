2b8ff8cac58fc5d4a1a8a13d7f4b08d005f3373d
shabanelmoogy <shaban.cec@gmail.com>
2025-10-04 11:09:23 +0300

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.07p3g3j0u78"
+    "revision": "0.ev33bcnre5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
