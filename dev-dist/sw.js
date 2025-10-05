b3b3e9ea730c3a4721f2064b9b44e035df6e7110
shabanelmoogy <shaban.cec@gmail.com>
2025-10-05 11:50:04 +0300

    update to 19.2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bnn9r4gfta"
+    "revision": "0.ler60n7vgl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
