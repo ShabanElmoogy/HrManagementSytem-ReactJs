eef606b689dc26df9e83b9f70b2a85e63e1959d6
shabanelmoogy <shaban.cec@gmail.com>
2025-10-21 10:57:53 +0300

    delete filemanager

--- src/features/FileManager/mediaViewer/types/mediaViewer.type.ts
+++
@@ -1,9 +0,0 @@
-export type MediaType = "iframe" | "image" | "video" | "audio" | "unsupported";
-
-export interface MediaContentProps {
-  mediaType: MediaType;
-  mediaUrl: string;
-  isLoading: boolean;
-  getFileExtension: () => string;
-  onError: (message: string) => void;
-}
